aeab4d32400466626f06de9190f23ec516d6ea2a
didimanoso <didi.manoso95@gmail.com>
2018-01-30 03:03:01 +0800

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,8 +21,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/11/cloud-hybrid-best-solution-for-company.html"}
-if(x=="2"){xxx="/2018/01/dedicated-server-or-vps-server.html"}
+if(x=="1"){xxx="/2018/01/dedicated-server-or-vps-server.html"}
+if(x=="2"){xxx="/2017/11/cloud-hybrid-best-solution-for-company.html"}
 if(x=="3"){xxx="/2018/01/basi-difference-between-hosting.html"}
 if(x=="4"){xxx="/2017/10/build-small-business-webhosting-with-vps.html"}
 if(x=="5"){xxx="/2018/01/top-25-big-data-companies.html"}a_to_ve[i].href="http://www.linkprotector.download"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
